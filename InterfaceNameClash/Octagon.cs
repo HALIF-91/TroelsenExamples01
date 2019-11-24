94fdedede0dc4bea75c75e0860f413cc8daacb58
ADIL <globalmail91@gmail.com>
2019-11-25 00:38:52 +0600

    явная реализация интерфейсов

---
+++ InterfaceNameClash/Octagon.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceNameClash
+{
+    class Octagon : IDrawToForm, IDrawToMemory, IDrawToPrinter
+    {
+        // чтобы избежать конфликта из за одинакового метода Draw() во всех интерфейсах, используется
+        // явная реализации интерфейса, не указывается модификатор доступа
+        void IDrawToForm.Draw()
+        {
+            Console.WriteLine("Drawing to form...");
+        }
+        void IDrawToMemory.Draw()
+        {
+            Console.WriteLine("Drawing to memory...");
+        }
+        void IDrawToPrinter.Draw()
+        {
+            Console.WriteLine("Drawing to printer...");
+        }
+    }
+}
---
+++ InterfaceNameClash/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceNameClash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Octagon octagon = new Octagon();
+            IDrawToForm form = octagon;
+            form.Draw();
+
+            IDrawToMemory memory = octagon;
+            memory.Draw();
+
+            IDrawToPrinter printer = octagon;
+            printer.Draw();
+
+            Console.ReadLine();
+        }
+    }
+}
